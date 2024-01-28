3e60e98ca6d592540a9ab1d4070dc5694071e976
ZigulatNatria <vachrameev.oleg@yandex.ru>
2024-01-28 22:14:14 +0500

    Добавлено лого и изображение продукта в админке

--- shop/showcase/admin.py
+++ shop/showcase/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Product, Category, Comment
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'text', 'price', 'amount', 'image', 'productCategory', 'units')
-    list_display_links = ('name', 'text', 'price', 'amount', 'image', 'productCategory', 'units')
+    list_display = ('name', 'text', 'price', 'amount', 'get_image', 'productCategory', 'units')
+    list_display_links = ('name', 'text', 'price', 'amount', 'get_image', 'productCategory', 'units')
     search_fields = ('name', 'text')
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="130" height="100"')
+
+    get_image.short_description = 'Изображение'
 
 
 class CategoryAdmin(admin.ModelAdmin):
